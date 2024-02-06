b561952e50b3f9f96b7576f3c2c813e1a849956c
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2024-02-07 00:23:47 +0700

    fix: NEXT_PUBLIC_VERCEL_URL config

--- constants/url.ts
+++ constants/url.ts
@@ -1,4 +1,3 @@
-export const GRAPHQL_API_URL =
-  process.env.NODE_ENV === "development"
-    ? `${process.env.NEXT_PUBLIC_SERVER}/api/graphql`
-    : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
+export const GRAPHQL_API_URL = process.env.NEXT_PUBLIC_SERVER
+  ? `${process.env.NEXT_PUBLIC_SERVER}/api/graphql`
+  : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;

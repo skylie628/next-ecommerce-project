69c42555636aafe416f01149567f0aa75eb62021
skylie628 <khuong.dinhbkk17@hcmut.edu.vn>
2024-02-07 00:17:32 +0700

    feat: building url

--- constants/url.ts
+++ constants/url.ts
@@ -1,1 +1,4 @@
-export const GRAPHQL_API_URL = `${process.env.NEXT_PUBLIC_SERVER}/api/graphql`;
+export const GRAPHQL_API_URL =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.NEXT_PUBLIC_SERVER}/api/graphql`
+    : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`;
